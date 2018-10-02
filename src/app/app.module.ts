5803c2472dcfe87f0bb1fcf5296e1a045fffffb5
alumno Secundaria <al3201@cedi.local>
2018-10-02 08:34:30 -0500

    Agregamos la funcion faltante y el horario y prfesor a la nueva pagina creada.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HistoriaPage } from '../pages/historia/historia';
 
 @NgModule({
   declarations: [
